a4e9345ff53e4b861ad056a16b66a8c13128833a
seaG7 <neyro.yt@gmail.com>
2024-10-13 23:13:46 +0300

    shader

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,8 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using TMPro.EditorUtilities;
-using Unity.VisualScripting;
-using Unity.XR.CoreUtils.GUI;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
